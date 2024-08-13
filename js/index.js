9b513494a53cfd2f39a758d587c0636521ba3468
Odissey7 <agapidu@inbox.ru>
2024-08-13 21:54:11 +0300

    fin 1

--- js/index.js
+++ js/index.js
@@ -263,29 +263,29 @@ document.querySelector('.phone_pop_1').onclick = function () {
 
 
 
-// Получаем элементы слайдера
-const slider = document.querySelector('.slider');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-const slides = Array.from(slider.querySelectorAll('img'));
-const slideCount = slides.length;
-let slideIndex = 0;
+// // Получаем элементы слайдера
+// const slider = document.querySelector('.slider');
+// const prevButton = document.querySelector('.prev-button');
+// const nextButton = document.querySelector('.next-button');
+// const slides = Array.from(slider.querySelectorAll('img'));
+// const slideCount = slides.length;
+// let slideIndex = 0;
 
-// Устанавливаем обработчики событий для кнопок
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
+// // Устанавливаем обработчики событий для кнопок
+// prevButton.addEventListener('click', showPreviousSlide);
+// nextButton.addEventListener('click', showNextSlide);
 
-// Функция для показа предыдущего слайда
-function showPreviousSlide() {
-slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-updateSlider();
-}
+// // Функция для показа предыдущего слайда
+// function showPreviousSlide() {
+// slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+// updateSlider();
+// }
 
-// Функция для показа следующего слайда
-function showNextSlide() {
-slideIndex = (slideIndex + 1) % slideCount;
-updateSlider();
-}
+// // Функция для показа следующего слайда
+// function showNextSlide() {
+// slideIndex = (slideIndex + 1) % slideCount;
+// updateSlider();
+// }
 
 // Функция для обновления отображения слайдера
 // function updateSlider() {
@@ -298,18 +298,18 @@ updateSlider();
 // });
 // }
 
-function updateSlider() {
-slides.forEach((slide, index) => {
-    if (index === slideIndex) {
-    slide.classList.remove('slider_img_hidden');
-    } else {
-        slide.classList.add('slider_img_hidden');
-    }
-});
-}
+// function updateSlider() {
+// slides.forEach((slide, index) => {
+//     if (index === slideIndex) {
+//     slide.classList.remove('slider_img_hidden');
+//     } else {
+//         slide.classList.add('slider_img_hidden');
+//     }
+// });
+// }
 
-// Инициализация слайдера
-updateSlider();
+// // Инициализация слайдера
+// updateSlider();
 
 
 // document.addEventListener("DOMContentLoaded", function () {
